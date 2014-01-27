d0227202f57d45cd7f43a9d72c6866d218c9c23a
ClarkLin <linchengkuang@foxmail.com>
2014-01-27 17:35:55 +0800

    Made some adjustment on pythoninstall.sh
    1. Added Python version and OS version on the title.
    2. Added "--no-check-certificate" in line 19 so that the server will
    download the Python files without certification check.
    3. Changed "temp install directory" to "temp directory".

--- pythoninstall.sh
+++ pythoninstall.sh
@@ -2,8 +2,8 @@
 # Created by Clark 2014.01.26
 # Contact Emaill: linchengkuang@foxmail.com
 # Name: pythoninstall.sh
-# This script is used to install Python
-# Version 1.0
+# This script is used to install Python 2.7 on Centos 6.3
+# Version 1.1
 # Refer to https://github.com/0xdata/h2o/wiki/Installing-python-2.7-on-centos-6.3.-Follow-this-sequence-exactly-for-centos-machine-only
 
 # Install development tools
@@ -16,7 +16,7 @@ mkdir pyinstall
 cd pyinstall
 
 # Download, compile and install Python
-wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
+wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2 --no-check-certificate
 tar -xjf Python-2.7.3.tar.bz2
 cd Python-2.7.3
 ./configure --prefix=/usr/local
@@ -27,11 +27,11 @@ make && make altinstall
 
 # Install and configuring distribute
 cd ~/pyinstall
-wget https://pypi.python.org/packages/source/d/distribute/distribute-0.6.27.tar.gz --no-check-certificate
+wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.27.tar.gz --no-check-certificate
 tar -xzf distribute-0.6.27.tar.gz
 cd distribute-0.6.27
 python2.7 setup.py install
 
-# Delete temp install directory
+# Delete temp directory
 cd ~
 rm pyinstall -rf
