74e7dc0ffa1d65df1092f157c45c56c888d47cab
Clark Lin <linchengkuang@foxmail.com>
2016-03-30 14:18:18 +0800

    Update centos_vnc_gnome_install.sh

--- centos_vnc_gnome_install.sh
+++ centos_vnc_gnome_install.sh
@@ -7,6 +7,7 @@ function GetVncPasswd(){
         echo "This script is used to install Gnome Desktop and VNC service on Ubuntu 14.04"
         echo "----------------------------------"
         read -p "Please enter the VNC password you want (default: vncuser): " VNC_PW
+        read -p "Please enter the VNC port you want (default: 74): " VNC_PORT
 }
 
 
@@ -22,30 +23,39 @@ function InstallGnome(){
 
 function InstallVnc(){
 	yum install -y tigervnc-server
-	cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:74.service
-	sed -i 's#<USER>#vncuser#g' /etc/systemd/system/vncserver@:74.service
+	cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:$VNC_PORT.service
+	sed -i 's#<USER>#vncuser#g' /etc/systemd/system/vncserver@:$VNC_PORT.service
 	systemctl daemon-reload
 }
 
 function InstallBrower(){
-	wget bm.jacashop.com/download/google-chrome-stable_current_x86_64.rpm
+	if [[ ! -a "google-chrome-stable_current_x86_64.rpm" ]]; then
+			wget bm.jacashop.com/download/google-chrome-stable_current_x86_64.rpm
+	fi
 	yum localinstall -y google-chrome-stable_current_x86_64.rpm
 }
 
 function ConfigVnc(){
 	(echo $VNC_PW && echo $VNC_PW) | adduser vncuser
-	su - vncuser -c "(echo $VNC_PW && echo $VNC_PW) | vncserver :74"
-	su - vncuser -c "vncserver -kill :74"
+	su - vncuser -c "(echo $VNC_PW && echo $VNC_PW) | vncserver :$VNC_PORT"
+	su - vncuser -c "vncserver -kill :$VNC_PORT"
 	yum update -y
 }
 
 function StartVnc(){
-	sudo systemctl enable vncserver@:74.service
-	sudo systemctl start vncserver@:74.service
-	cp /usr/share/applications/google-chrome.desktop /home/vncuser/Desktop/
-	cp /usr/share/applications/libreoffice-writer.desktop /home/vncuser/Desktop/
-	cp /usr/share/applications/libreoffice-calc.desktop /home/vncuser/Desktop/
+	sudo systemctl enable vncserver@:$VNC_PORT.service
+	sudo systemctl start vncserver@:$VNC_PORT.service
+	if [[ ! -a "/home/vncuser/Desktop/google-chrome.desktop" ]]; then
+		cp /usr/share/applications/google-chrome.desktop /home/vncuser/Desktop/
+	fi
+	if [[ ! -a "/home/vncuser/Desktop/libreoffice-writer.desktop" ]]; then
+		cp /usr/share/applications/libreoffice-writer.desktop /home/vncuser/Desktop/
+	fi
+	if [[ ! -a "/home/vncuser/Desktop/libreoffice-calc.desktop" ]]; then
+		cp /usr/share/applications/libreoffice-calc.desktop /home/vncuser/Desktop/
+	fi
 	chown vncuser -Rf /home/vncuser/Desktop/*
+	chmod a+x /home/vncuser/Desktop/*.desktop
 }
 
 function Main(){
