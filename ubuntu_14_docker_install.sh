0742b8a25c7be2f98d8e9ca23eb2d86c6fde2a0b
Clark Lin <linchengkuang@foxmail.com>
2016-03-30 09:39:47 +0800

    Update ubuntu_14_docker_install.sh

--- ubuntu_14_docker_install.sh
+++ ubuntu_14_docker_install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo update-ca-certificates
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y docker-engine
